cccc9e5c0c3667fe7d8ed94e2d09138567dbee3d
Fredy Coronado <fecoronador9@misena.edu.co>
2020-03-27 19:17:58 -0500

    Laravel dockerizado

---
+++ proyecto/index.php
@@ -0,0 +1,16 @@
+<?php
+echo 'hola php locooooo
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, exercitationem iusto sequi vero numquam aut amet maiores obcaecati ducimus repudiandae distinctio possimus cum asperiores vitae delectus. Impedit, numquam? Ratione, soluta.';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nativo</title>
+</head>
+<body>
+    <h1>Hola mundo</h1>
+   
+</body>
+</html>
